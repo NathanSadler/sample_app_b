c16dd0fb1ea3e6d17deeca573b581935ffb21a94
Nathan Sadler <nathan@sadlerville.com>
2021-07-06 09:48:45 -0400

    before trying to install pry gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new(name: "Test user", email: "user@example.com")}
+
+  it("is initially valid") do
+    expect(user.valid?).to(eq(true))
+  end
+
+  it("should have an email present") do
+    user.name = "    "
+    binding.pry
+    expect(user.valid?).to(eq(false))
+  end
 end
