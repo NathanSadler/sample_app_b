5faacff34964e82a1ab00678ad9d9179dd329ad5
Nathan Sadler <nathan@sadlerville.com>
2021-07-07 09:33:18 -0400

    add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.new(user_params)
     if @user.save
-      # handle successful save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
---
+++ spec/requests/users_signups_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "UsersSignups", type: :request do
+  describe "GET /users_signups" do
+    it "doesn't accept invalid submissions" do
+      get signup_path
+      assert_no_difference 'User.count' do
+        post users_path, params: { user: {name: " ", email: "user@invalid",
+          password: "foo", password_confirmation: "bar"}}
+      end
+    end
+    it "accepts valid submissions" do
+      get signup_path
+      assert_difference 'User.count', 1 do
+        post users_path, params: { user: {name: "Example User",
+          email: "user@example.com", password: "password", password_confirmation: "password"}}
+      end
+      follow_redirect!
+      expect(request.path).to(eq('/users/1'))
+    end
+  end
+end
