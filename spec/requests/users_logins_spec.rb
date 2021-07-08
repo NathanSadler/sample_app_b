9f56dbb2842ab3090ee3871ab0ed7164e5fdc18a
Nathan Sadler <nathan@sadlerville.com>
2021-07-08 09:43:13 -0400

    got dropdown menu to work

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "UsersLogins", type: :request do
   describe "GET /users_logins" do
+    fixtures :users
+
+    let(:user) {users(:michael)}
+
     it "doesn't display a flash when the user moves to another page" do
       get login_path
       expect(request.path).to(eq('/login'))
@@ -10,6 +14,13 @@ RSpec.describe "UsersLogins", type: :request do
       expect(flash.empty?).to(eq(false))
       get root_path
       expect(flash.empty?).to(eq(true))
+    end
+
+    it "logs in with valid information" do
+      get login_path
+      post login_path, params: { session: { email: user.email,
+                                        password: 'password' } }
+      assert_redirected_to user
     end
   end
 end
