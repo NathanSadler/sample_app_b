742777a73e7deb155968f4befc09bf9cef2d5b83
Nathan Sadler <nathan@sadlerville.com>
2021-07-08 15:16:52 -0400

    ???

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,12 @@ module SessionsHelper
     end
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def logged_in?
     !current_user.nil?
   end
