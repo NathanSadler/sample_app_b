97818f274b45b52f571338683cd99a4e93380d84
Nathan Sadler <nathan@sadlerville.com>
2021-08-05 14:25:31 -0400

    add test for making unsuccessful edits

--- spec/support/helpers/fill_in_forms.rb
+++ spec/support/helpers/fill_in_forms.rb
@@ -3,8 +3,21 @@ module FillInForms
     session.fill_in 'Name', with: name
     session.fill_in 'Email', with: email
     session.fill_in 'Password', with: password
-    if password_confirmation.nil?
-      session.fill_in 'Password Confirmation'
-    end
+  end
+
+  def submit_user_form(session:, name:, email:, password:, button_text:, password_confirmation: nil)
+  fill_in_user_form(session, name, email, password, password_confirmation = nil)
+    session.click_on(button_text)
+  end
+
+  def fill_in_login_form(session:, email:, password:, remember_me: false)
+    session.fill_in 'Email', with: email
+    session.fill_in 'Password', with: password
+  end
+
+  def submit_login_form(session:, email:, password:, remember_me: false)
+    fill_in_login_form(session: session, email: email, password: password)
+    session.check("Remember me on this computer") if remember_me
+    session.click_on("Log In")
   end
 end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -7,8 +7,12 @@ RSpec.describe "User", type: :system do
 
   describe("user trying to update a user") do
     it("keeps the user on the update user page if their update is unsuccessful") do
+      session.visit(login_path)
+      submit_login_form(session: session, email: user.email, password: user.password)
       session.visit(edit_user_path(user))
-      
+      submit_user_form(session: session, name: " ", email:"foo@invalid", password:"foo", password_confirmation: "bar", 
+        button_text: "Save Changes")
+      expect(session.current_path).to(eq("/users/#{user.id}"))
     end
   end
 end
