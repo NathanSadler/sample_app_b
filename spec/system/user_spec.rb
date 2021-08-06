f0df940c6185703dc15e485b24a36668209a1325
Nathan Sadler <nathan@sadlerville.com>
2021-08-06 13:49:23 -0400

    add tests for when a logged in user tries to edit another user

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -34,6 +34,25 @@ RSpec.describe "User", type: :system do
       expect(session2.has_content?("Please log in.")).to(be(true))
     end
 
+    describe("a logged in user trying to update a different user") do
+
+        let(:session2) {Capybara::Session.new(:rack_test, Rails.application)}
+
+      before(:each) do
+        session2.visit(login_path)
+        submit_login_form(session: session2, email: "duchess@example.gov", password: "password")
+        session2.visit(edit_user_path(user))
+      end
+
+      it("returns the logged-in user to the homepage") do
+        expect(session2.current_path).to(eq("/"))
+      end
+
+      it("doesn't display a flash") do
+        expect(session2.body).to_not(have_content?('class="alert '))
+      end
+    end
+
     describe("with valid information") do
       before(:each) do
         submit_user_form(session: session, name: "Foo Bar", email: "foo@bar.com", password: "", 
@@ -46,8 +65,8 @@ RSpec.describe "User", type: :system do
       end
 
       it("updates their information") do
-        expect(User.last.name).to(eq("Foo Bar"))
-        expect(User.last.email).to(eq("foo@bar.com"))
+        expect(User.first.name).to(eq("Foo Bar"))
+        expect(User.first.email).to(eq("foo@bar.com"))
       end
     end
   end
