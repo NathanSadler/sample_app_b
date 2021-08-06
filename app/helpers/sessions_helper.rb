1580631a73a75f8cb48d6a84e4e9a993030a5449
Nathan Sadler <nathan@sadlerville.com>
2021-08-06 16:34:24 -0400

    implement friendly redirecting

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -60,6 +60,6 @@ module SessionsHelper
 
   # stores the url trying to be accessed
   def store_location
-    session[:forwarding_url] = request.original if request.get?
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
