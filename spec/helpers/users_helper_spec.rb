918d466c169b8a7fd2a090a065171d31cd79f911
Nathan Sadler <nathan@sadlerville.com>
2021-08-10 16:56:59 -0400

    comment out auto-generated tests

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -19,6 +19,6 @@ def log_in_as(user)
   session[:user_id] = user.id
 end
 
-RSpec.describe UsersHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe UsersHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
