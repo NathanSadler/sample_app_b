7ae1075b73a1fda52ea4cd6d6f452aa5661d16b6
Nathan Sadler <nathan@sadlerville.com>
2021-08-12 16:57:18 -0400

    sessions_controller.create  will now log in users as normal OR redirect to root with warning if the user is not activated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,27 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      # Log user in and redirect to user's show page
-      reset_session
-      # binding.pry
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      # remember(user)
-      log_in user
-      redirect_to forwarding_url || user
-      # redirect_to user
+      if user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        log_in user
+        redirect_to forwarding_url || user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
+      # forwarding_url = session[:forwarding_url]
+      # # Log user in and redirect to user's show page
+      # reset_session
+      # # binding.pry
+      # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      # # remember(user)
+      # log_in user
+      # redirect_to forwarding_url || user
+      # # redirect_to user
     else
       # Create an error message
       flash.now[:danger] = "Invalid email/password combination"
