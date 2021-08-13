acbcf24dfcf9f4b1a6646924481f8a6e55be1422
Nathan Sadler <nathan@sadlerville.com>
2021-08-13 16:30:45 -0400

    add some tests

--- spec/system/user_signups_spec.rb
+++ spec/system/user_signups_spec.rb
@@ -1,26 +1,42 @@
 require 'rails_helper'
 require_relative '../helpers/users_helper_spec'
 
-RSpec.describe "UsersSignups", type: :system do
+RSpec.describe 'UsersSignups', type: :system do
+
+  let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
+
   before(:each) do
     ActionMailer::Base.deliveries.clear
   end
 
-  let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
-
-  describe("signing up with invalid information") do
-
-    it("doesn't create a new user") do
-      expect { sign_up(
-        session: session, 
-        name: "", 
-        email: "user@invalid", 
-        password: "foo", 
-        password_confirmation: "bar")
-     }.to change {User.count}.by(0)
+  context 'signing up with invalid information' do
+    def sign_up_with_session(current_session)
+      sign_up(
+        session: current_session,
+        name: '',
+        email: 'user@invalid', 
+        password: 'foo', 
+        password_confirmation: 'bar'
+      )
     end
 
-    
+    before(:each) do
+      sign_up_with_session(session)
+    end
 
+    it "doesn't create a new user" do
+      expect {sign_up_with_session(session)}.to_not change { User.count }
+    end
+
+    it 'displays a div listing the errors' do
+      error_explanations = session.all('div#error_explanation')
+      expect(error_explanations.length).to be > 0
+      expect(error_explanations[0].text.empty?).to(be(false))
+    end
+
+    it "creates divs with class 'field_with_errors'" do
+      expect(session.all('div.field_with_errors').length).to be > 0
+    end
   end
 end
+
