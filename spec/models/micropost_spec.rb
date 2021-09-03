a0302331bfec5ba57aa556059299c589ec8be830
Nathan Sadler <nathan@sadlerville.com>
2021-09-03 09:09:24 -0400

    add some validations for micropost model

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :users
+  let(:user) {users(:michael)}
+
+  let(:micropost) {Micropost.new(content: 'Lorem ipsum', user_id: user.id)}
+
+  context('validations') do
+    it('should succeed when given content and a user id') do
+      expect(micropost.valid?).to(be(true))
+    end
+
+    it('should fail when not given a user id') do
+      micropost.user_id = nil
+      binding.pry
+      expect(micropost.valid?).to(be(false))
+    end
+  end
 end
