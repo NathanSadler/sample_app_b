cb5083dd66555d9b9c1a87f12de8115fefb66d01
Nathan Sadler <nathan@sadlerville.com>
2021-09-04 14:01:31 -0400

    add logged_in_user to micropost controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+  # might want to remove logged_in_user from the Users controller if this causes any problems
+  def logged_in_user
+    if !logged_in?
+      store_location
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
 end
