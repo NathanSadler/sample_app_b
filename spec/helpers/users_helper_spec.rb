7e367c9edbc90cd30b4cdb5ec34c9e6f0f7f9533
Nathan Sadler <nathan@sadlerville.com>
2021-09-13 09:14:48 -0400

    add active storage?

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -15,8 +15,14 @@ def is_logged_in?
   !session[:user_id].nil?
 end
 
-def log_in_as(user)
-  session[:user_id] = user.id
+# def log_in_as(user)
+#   session[:user_id] = user.id
+# end
+
+def log_in_as(user, password: 'password', remember_me: '1')
+  post login_path, params: { session: { email: user.email,
+                                        password: password,
+                                        remember_me: remember_me } }
 end
 
 # RSpec.describe UsersHelper, type: :helper do
--- spec/system/micropost_controller_spec.rb
+++ spec/system/micropost_controller_spec.rb
@@ -3,6 +3,7 @@ require_relative '../helpers/users_helper_spec'
 
 RSpec.describe "Micropost controller", type: :request do
   fixtures :microposts
+  fixtures :users
 
   let(:micropost) {microposts(:orange)}
 
@@ -17,6 +18,16 @@ RSpec.describe "Micropost controller", type: :request do
     it ('should redirect when not logged in') do
       expect {delete micropost_path(micropost)}.not_to change {Micropost.count}
       assert_redirected_to login_url
+    end
+  end
+
+  context 'trying to destroy the wrong micropost' do
+    it('should redirect') do
+      log_in_as(users(:michael))
+      micropost = microposts(:ants)
+      # expect()
+      expect {delete micropost_path(micropost)}.not_to change {Micropost.count}
+      assert_redirected_to root_url
     end
   end
 end
