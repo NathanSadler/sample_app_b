f108bedf5b32e5d84538566af0f3d2eab1b365ec
Nathan Sadler <nathan@sadlerville.com>
2021-09-29 19:00:55 -0400

    add validations for relationship model

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :users
+
+  let(:relationship) {Relationship.new(follower_id: users(:michael).id, followed_id: users(:archer).id)}
+
+  it('should be valid') do
+    expect(relationship.valid?).to(eq(true))
+  end
+
+  it('should require a follower_id') do
+    relationship.follower_id = nil
+    expect(relationship.valid?).to(eq(false))
+  end
+
+  it('should require a followed_id') do
+    relationship.followed_id = nil
+    expect(relationship.valid?).to(eq(false))
+  end
 end
