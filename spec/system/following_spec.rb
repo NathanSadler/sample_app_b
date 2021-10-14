0a8d062b05444a2bc704a2f69d86dacbc8a3b85b
Nathan Sadler <nathan@sadlerville.com>
2021-10-14 16:40:11 -0400

    follow/unfollow button tests

--- spec/system/following_spec.rb
+++ spec/system/following_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "following", type: :system do
   fixtures :relationships
 
   let(:user) {users(:michael)}
+  let(:other) {users(:archer)}
 
   before(:each) do
     log_in_as(user)
@@ -31,6 +32,34 @@ RSpec.describe "following", type: :system do
     assert_match user.followers.count.to_s, response.body
     user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  describe("following a user without Ajax") do
+    it("should increase the followed user's followers count by 1") do
+      expect {post relationships_path, params: {followed_id: other.id}}.to change {user.following.count}.by(1)
+    end
+  end
+
+  describe("following a user with Ajax") do
+    it("should increase the followed user's followers count by 1") do
+      expect {post relationships_path, xhr: true, params: {followed_id: other.id}}.to change {user.following.count}.by(1)
+    end
+  end
+
+  describe("unfollowing a user without ajax") do
+    it("should decrease the unfollowed user's follower count by one") do
+      user.follow(other)
+      relationship = user.active_relationships.find_by(followed_id: other.id)
+      expect {delete relationship_path(relationship)}.to change{user.following.count}.by(-1)
+    end
+  end
+
+  describe("unfollowing a user with ajax") do
+    it("should decrease the unfollowed user's follower count by one") do
+      user.follow(other)
+      relationship = user.active_relationships.find_by(followed_id: other.id)
+      expect {delete relationship_path(relationship), xhr: true}.to change{user.following.count}.by(-1)
     end
   end
 end
