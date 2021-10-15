7fc58d57238c7a9f65a7b0fccd874a8b98d0815c
Nathan Sadler <nathan@sadlerville.com>
2021-10-15 17:01:50 -0400

    reran rolemodel css generator

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,6 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// require custom icons
+require.context('images/icons', true)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.insert(
+  'customIcons',
+  {
+    test: /\/icons\/.*.svg$/,
+    use: [{
+      loader: require.resolve('./loaders/custom-icon-loader')
+    }]
+  },
+  { after: 'file' }
+)
+
+environment.loaders.get('file').exclude = /\/icons\/.*.svg$/
+
 module.exports = environment
