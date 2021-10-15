291ebda20cbc6da2eba781f4d0bb41febd78deff
Nathan Sadler <nathan@sadlerville.com>
2021-10-15 12:36:59 -0400

    fix location of method

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,10 +4,10 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validates :image, content_type: {in: %w[image/jpeg image/gif image/png], message: 'must be a valid file format'}, 
+  validates :image, content_type: {in: %w[image/jpeg image/gif image/png], message: 'must be a valid file format'},
   size: {less_than: 5.megabytes, message: 'should be less than 5MB'}
-end
 
-def display_image
-  image.variant(resize_to_limit: [500, 500])
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
